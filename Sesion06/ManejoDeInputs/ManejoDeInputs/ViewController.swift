0be28e4567db36fa350fa070e8bcbd6237ba2361
Kenyi Rodriguez <krodriguez@solera.pe>
2020-10-02 21:35:59 -0500

    Sesion06 completa

--- Sesion06/ManejoDeInputs/ManejoDeInputs/ViewController.swift
+++ Sesion06/ManejoDeInputs/ManejoDeInputs/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtName      : UITextField!
+    @IBOutlet weak var txtDocument  : UITextField!
+    
+    @IBAction func tapToClodeKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setStyleToTextField(self.txtName, ifIsSelect: false)
+        self.setStyleToTextField(self.txtDocument, ifIsSelect: false)
     }
+    
+    func setStyleToTextField(_ textField: UITextField, ifIsSelect isSelect: Bool) {
+        
+        textField.backgroundColor = .white
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = isSelect ? UIColor.systemBlue.cgColor : UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 10
+        
+        textField.layer.shadowColor = UIColor.black.cgColor
+        textField.layer.shadowOffset = CGSize(width: 0, height: 0)
+        textField.layer.shadowRadius = 2
+        textField.layer.shadowOpacity = 0.2
+        
+    }
+    
+}
 
+extension ViewController: UIScrollViewDelegate {
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.setStyleToTextField(textField, ifIsSelect: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.setStyleToTextField(textField, ifIsSelect: false)
+    }
 }
 
