f7af368d5a40d232076fad8e24d4dd6e5882dc78
Kenyi Rodriguez <krodriguez@solera.pe>
2020-11-06 19:19:09 -0500

    Inicio Sesión 11

---
+++ Sesion11/DemoCocoaPods/DemoCocoaPods/MapViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  DemoCocoaPods
+//
+//  Created by Kenyi Rodriguez on 10/23/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapVIew: GMSMapView!
+    
+    var objPlace: PlaceBE!
+    
+    lazy var locationManager: CLLocationManager = {
+       
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.distanceFilter = 2000//Esta en metros
+        locationManager.requestWhenInUseAuthorization()
+        return locationManager
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mapVIew.settings.myLocationButton = true
+        self.mapVIew.settings.compassButton = true
+        self.mapVIew.isMyLocationEnabled = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        self.createMarkerAtPosition(self.objPlace.place_coordinate)
+    }
+    
+    func createMarkerAtPosition(_ coordinatePosition: CLLocationCoordinate2D) {
+        
+        let marker = GMSMarker(position: coordinatePosition) //se crear el pin
+        marker.map = self.mapVIew// se agrega el pin al mapa
+        
+        let camera = GMSCameraPosition(target: coordinatePosition, zoom: 16)
+        self.mapVIew.animate(to: camera)
+    }
+    
+}
+
+extension MapViewController: GMSMapViewDelegate {
+
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+    
+        return true
+    }
+}
---
+++ Sesion11/DemoCocoaPods/DemoCocoaPods/PlacesViewController.swift
@@ -0,0 +1,121 @@
+//
+//  PlacesViewController.swift
+//  DemoCocoaPods
+//
+//  Created by Kenyi Rodriguez on 10/30/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class PlacesViewController: UIViewController {
+    
+    var arrayPlaces = [PlaceBE]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.arrayPlaces.append(PlaceBE(name: "ISIL Miraflores",
+                                        address: "Av. Alfredo Benavides 778, Cercado de Lima 15074",
+                                        coordinate: CLLocationCoordinate2D(latitude: -12.125423, longitude: -77.024843),
+                                        urlImage: ""))
+        
+        self.arrayPlaces.append(PlaceBE(name: "ISIL San Isidro",
+                                        address: "Av. Faustino Sánchez Carrión 285, San Isidro 15076",
+                                        coordinate: CLLocationCoordinate2D(latitude: -12.093847, longitude: -77.052979),
+                                        urlImage: ""))
+        
+        self.arrayPlaces.append(PlaceBE(name: "ISIL La Molina",
+                                        address: "Av. la Fontana 955, La Molina 15024",
+                                        coordinate: CLLocationCoordinate2D(latitude: -12.073381, longitude: -76.948049),
+                                        urlImage: ""))
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let controller = segue.destination as? MapViewController {
+            controller.objPlace = sender as? PlaceBE
+        }
+    }
+}
+
+
+//Sirve para construir el contenido de la tabla
+extension PlacesViewController: UITableViewDataSource { //Tiene 3 metodos principales: number, number, cellFor
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayPlaces.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "PlaceTableViewCell" //Debe coincidir con el nombre del storyboard y es KeySensitive
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! PlaceTableViewCell
+        cell.objPlace = self.arrayPlaces[indexPath.row]
+        
+        return cell
+    }
+
+}
+
+//Sirve para detectar eventos o acciones que da el usuario en la tabla
+extension PlacesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let objPlace = self.arrayPlaces[indexPath.row]
+        self.performSegue(withIdentifier: "MapViewController", sender: objPlace)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var arrayMarcasAutos = [
+//    ["S3", "R8", "RS5", "TT RS"], //Audi
+//    ["A45", "C63 AMG", "C250", "SLX300"], //Meche
+//    ["M5", "Z4", "118i", "M2"] //BMW
+//]
+
+////Sirve para construir el contenido de la tabla
+//extension PlacesViewController: UITableViewDataSource { //Tiene 3 metodos principales: number, number, cellFor
+//
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return self.arrayMarcasAutos.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//        let arrayModelBrand = self.arrayMarcasAutos[section]
+//        return arrayModelBrand.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//}
